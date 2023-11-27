b588b0009fd1445fca2995bfad9c7519cfbd716c
LuanC14 <luanchr14@gmail.com>
2023-11-27 13:06:35 -0300

    Add:Local storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,10 +1,17 @@
-import { ReactNode, createContext, useReducer, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
 } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 interface CreateCycleData {
   task: string;
@@ -31,17 +38,38 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJson = localStorage.getItem(
+        "@ignite-timer:cycles-state-1.0.0"
+      );
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      if (storedStateAsJson) {
+        return JSON.parse(storedStateAsJson);
+      }
+
+      return initialState;
+    }
+  );
+
   const { cycles, activeCycleId } = cyclesState;
 
   const activeCycle = cyclesState.cycles.find(
     (cycle) => cycle.id == activeCycleId
   );
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
 
   function createNewCycle(data: CreateCycleData) {
     const newCycle: Cycle = {
@@ -65,6 +93,11 @@ export function CyclesContextProvider({
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds);
   }
+
+  useEffect(() => {
+    const stateJson = JSON.stringify(cyclesState);
+    localStorage.setItem("@ignite-timer:cycles-state-1.0.0", stateJson);
+  }, [cyclesState]);
 
   return (
     <CyclesContext.Provider
